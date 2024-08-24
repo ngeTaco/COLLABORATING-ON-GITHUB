5a4876b86750de652aea1545607d0f97cecd9b3b
Diego Hernandez <diego@udundi.com>
2024-08-23 23:48:39 -0600

    added public folders

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ export default function App() {
 
   return (
     <>
-      <nav className='bg-brand-blue-100 h-10'>
-        <section className='flex flex-row justify-evenly items-center text-center px-8 h-full'>
+      <nav className='bg-brand-blue-100 h-14 flex'>
+        <NavLink to="/">
+        <img className='h-full' src='/logo.jpg'/>
+        </NavLink>
+        <div className='flex flex-row justify-evenly items-center w-5/6 text-center px-8 h-full'>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/page-02">Page 02</NavLink>
             <NavLink to="/page-03">Page 03</NavLink>
-        </section>
+        </div>
       </nav>
 
 
